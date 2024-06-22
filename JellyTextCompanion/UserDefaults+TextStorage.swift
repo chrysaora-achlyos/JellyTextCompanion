64ca70db5c6466e40cb1efaf812a54bb3ff1b751
dev <mbpro.m1.dev@ma7.org>
2024-06-22 16:45:05 -0500

    clean up sloppy cut and paste resulting in lots of blank linkes.

--- JellyTextCompanion/UserDefaults+TextStorage.swift
+++ JellyTextCompanion/UserDefaults+TextStorage.swift
@@ -7,25 +7,15 @@
 
 import Foundation
 extension UserDefaults {
-
     private enum Key {
-
         static let text = "text"
-
     }
     var text: String {
-
         get {
-
             return string(forKey: Key.text) ?? ""
-
         }
-
         set {
-
             set(newValue, forKey: Key.text)
-
         }
-
     }
 }
